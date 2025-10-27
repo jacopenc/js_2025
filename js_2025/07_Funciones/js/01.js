08f1c4b325e78b25fbb5fa08ea3cf935a47bdb0a
jmacosta2001 <jmacosta2001@gmail.com>
2025-10-27 21:28:53 +0000

    add js

---
+++ js_2025/07_Funciones/js/01.js
@@ -0,0 +1,21 @@
+// 1. Función básica
+function saludar() {
+  console.log("¡Hola, mundo!");
+  alert("¡Hola, mundo!");
+}
+
+// 2. Función con parámetros
+function saludarNombre(nombre) {
+  console.log("¡Hola, " + nombre + "!");
+  alert("¡Hola, " + nombre + "!");
+}
+
+// 3. Función con retorno
+function sumar(a, b) {
+  return a + b;
+}
+function mostrarSuma() {
+  alert("La suma es: " + sumar(5, 3));
+}
+
+
